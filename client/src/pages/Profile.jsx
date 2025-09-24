a4ae9c9a2ee99c869848aba1406e86574c15b402
HafeezIlias <HafeezIlias.1@gmail.com>
2025-09-24 12:55:54 +0800

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,15 +2,17 @@ import React, { useRef, useEffect, useState } from 'react'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { useSelector ,useDispatch} from 'react-redux'
 import { app } from '../firebase.js';
-
+import { updateUserSuccess, updateUserStart, updateUserFailure } from '../redux/user/userSlice.js';
+import { set } from 'mongoose';
 export default function Profile() {
   const dispatch = useDispatch();
   const fileRef=useRef(null);
-  const {currentUser} = useSelector((state) => state.user);
+  const {currentUser,loading,error} = useSelector((state) => state.user);
   const [file,setFile] = useState(undefined); // file is like a const that holds the state, to update the state we need to use the setFile
   const [filePercent, setFilePercent] = useState(0); //initialize file upload progress to 0
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({}); // to hold the form data
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   //firebase storage
   //allow read
   //allow write: if request.resource.data.size < 2*1024*1024 && request.resource.data.contentType.matches('image/.*');
@@ -50,22 +52,52 @@ export default function Profile() {
     );
 }
 
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value}); //spread operator to copy the previous state and update the new state by the id of the input field
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault(); //to prevent the default behavior of the form
+    //send the form data to the backend to update the user profile
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`backend/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+        }),
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        return dispatch(updateUserFailure(data.message));
+      }
+      dispatch(updateUserSuccess(data)); //update the user in the redux store
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  }
   return (
     <div className=' bg-white p-3 max-w-lg mx-auto mt-5 rounded-xl shadow-lg'>
       <h1 className='text-3xl font-semibold text-center my-2'>Profile</h1>
-      <form className= 'flex flex-col gap-4'> {/*gap control the space between the elements*/}
+      <form className= 'flex flex-col gap-4' onSubmit={handleSubmit}> {/*gap control the space between the elements*/}
         <input onChange={(e) => setFile(e.target.files[0])} type="file"  ref={fileRef} hidden accept='image/*'/> {/* upload funcionality*/}
         <img onClick={() => fileRef.current.click()} className=" shadow-2xl w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2" src={currentUser.avatar} alt="profile" /> {/* src={formData.avatar || currentUser.avatar}if formData.avatar exist use it otherwise use currentUser.avatar..formData is newly added later sign in it will use the currentUser.avatar*/}
         <p className='text-sm self-center'>{fileUploadError ? (<span className='text-red-600'>Error uploading file(less than 2mb)</span>) : filePercent > 0 && filePercent < 100 ? (<span className='text-slate-600'>Uploading {filePercent}%</span>) : filePercent === 100 ? (<span className='text-green-500'>Upload Complete</span>) : ""}</p>
-        <input className="shadow-xl p-3 rounded-xl"type="text" placeholder='username' id='username' />
-        <input className="shadow-xl p-3 rounded-xl"type="email" placeholder='email' />
+        <input className="shadow-xl p-3 rounded-xl"type="text" placeholder='username' id='username' defaultValue={currentUser.username}onChange={handleChange}/>
+        <input className="shadow-xl p-3 rounded-xl"type="email" placeholder='email' defaultValue={currentUser.email}onChange={handleChange}/>
         <input className="shadow-xl p-3 rounded-xl"type="password" placeholder='password' />
-        <button className='bg-slate-600 text-white p-3 rounded-xl uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+        <button className='bg-slate-600 text-white p-3 rounded-xl uppercase hover:opacity-95 disabled:opacity-80 ' disabled={loading} >{loading? "Loading ...": "Update"}</button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-600 cursor-pointer'> Delete Account</span>
         <span className='text-red-600 cursor-pointer'> Sign Out</span>
       </div>
+      <p className='text-red-600 mt-5'>{error ? error : ""}</p>
+      <p className='text-green-600 mt-5'>{updateSuccess ? "Updated Successfully" : ""}</p>
     </div>
   )
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -18,13 +18,25 @@ const userSlice = createSlice({
           state.currentUser = action.payload; //payload is the data that we send from the backend (user data)
           state.error = null;
       },
-        signinFailure:(state,action) => {
-          state.loading = false;
-          state.error = action.payload;
-    },
+      signinFailure:(state,action) => {
+        state.loading = false;
+        state.error = action.payload;
+      },
+      updateUserStart:(state) => {
+        state.loading = true;
+      },
+      updateUserSuccess:(state,action) => {
+        state.loading = false;
+        state.currentUser = action.payload; //payload is the updated user data that we send from the backend
+        state.error = null;
+      },
+      updateUserFailure:(state,action) => {
+        state.loading = false;
+        state.error = action.payload;
+      },
     },
   });
   
-  export const {signinStart,signinSuccess,signinFailure} = userSlice.actions;
+  export const {signinStart,signinSuccess,signinFailure,updateUserStart,updateUserSuccess,updateUserFailure} = userSlice.actions;
   export default userSlice.reducer; //when default we can change the name while importing
   
