6fbb31fd8627a8b6adfc6cc506b258f50a8eb41b
HafeezIlias <HafeezIlias.1@gmail.com>
2025-10-01 22:20:13 +0800

    addd contact landlord functionality to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper";
-import { useSelector } from "react-redux";
-import { Navigation } from "swiper/modules";
-import "swiper/css/bundle";import {
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+import {
   FaBath,
   FaBed,
   FaChair,
@@ -13,10 +14,12 @@ import "swiper/css/bundle";import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
-import Contact from '../components/Contact';
+import Contact from '../components/Contact.jsx';
+
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
-    SwiperCore.use([Navigation]);
+  SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -24,17 +27,16 @@ export default function Listing() {
   const [contact, setContact] = useState(false);
   const params = useParams();
   const { currentUser } = useSelector((state) => state.user);
+
   useEffect(() => {
     const fetchListing = async () => {
       try {
         setLoading(true);
-        const res = await fetch(
-          `/backend/listing/getListing/${params.listingId}`
-        );
+        const res = await fetch(`/backend/listing/getListing/${params.listingId}`);
         const data = await res.json();
         if (data.success === false) {
-          setLoading(false);
           setError(true);
+          setLoading(false);
           return;
         }
         setListing(data);
@@ -50,28 +52,26 @@ export default function Listing() {
 
   return (
     <main>
-      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
       {error && (
-        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
       )}
       {listing && !loading && !error && (
-        <>
-          <Swiper navigation style={{'--swiper-navigation-color': 'white',
-          '--swiper-pagination-color': 'white',
-          }}>
+        <div>
+          <Swiper navigation>
             {listing.imageUrls.map((url) => (
-              <SwiperSlide key={url} >
+              <SwiperSlide key={url}>
                 <div
-                  className="h-[550px]"
+                  className='h-[550px]'
                   style={{
                     background: `url(${url}) center no-repeat`,
-                    backgroundSize: "cover",
+                    backgroundSize: 'cover',
                   }}
                 ></div>
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer '>
+          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
             <FaShare
               className='text-slate-500'
               onClick={() => {
@@ -90,7 +90,7 @@ export default function Listing() {
           )}
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
-              {listing.name} - RM{' '}
+              {listing.name} - ${' '}
               {listing.offer
                 ? listing.discountPrice.toLocaleString('en-US')
                 : listing.regularPrice.toLocaleString('en-US')}
@@ -114,7 +114,7 @@ export default function Listing() {
               <span className='font-semibold text-black'>Description - </span>
               {listing.description}
             </p>
-          <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+            <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
               <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaBed className='text-lg' />
                 {listing.bedrooms > 1
@@ -146,8 +146,7 @@ export default function Listing() {
             )}
             {contact && <Contact listing={listing} />}
           </div>
-          
-        </>
+        </div>
       )}
     </main>
   );
